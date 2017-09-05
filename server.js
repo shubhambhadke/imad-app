42b06901f077b70801d353bba37b9ec730a90c59
shubhambhadke <30976529+shubhambhadke@users.noreply.github.com>
2017-09-05 10:58:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 app.get('/article-one ',function(req,res){
-    res.send('Article one is displayed and served here ');
+     res.sendFile(path.join(__dirname ,'ui','article_one.html'));
     
 });
 
